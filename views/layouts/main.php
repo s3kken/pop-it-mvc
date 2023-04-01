58800ebe184cefab934712f15d13ebed7ecfc0ed
s3kken <your.unicorn0@gmail.com>
2023-04-01 06:10:37 +0300

    Функция аутентификации

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -5,13 +5,30 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Main site</title>
+    <title>Pop it MVC</title>
 </head>
 <body>
+<header>
+    <nav>
+        <a href="<?= app()->route->getUrl('/hello') ?>">Главная</a>
+        <?php
+        if (!app()->auth::check()):
+            ?>
+            <a href="<?= app()->route->getUrl('/login') ?>">Вход</a>
+            <a href="<?= app()->route->getUrl('/signup') ?>">Регистрация</a>
+        <?php
+        else:
+            ?>
+            <a href="<?= app()->route->getUrl('/logout') ?>">Выход (<?= app()->auth::user()->name ?>)</a>
+        <?php
+        endif;
+        ?>
+    </nav>
+</header>
 
-<div>
-    <?= $content ?? ''; ?>
-</div>
+<main>
+    <?= $content ?? '' ?>
+</main>
 
 </body>
 </html>
